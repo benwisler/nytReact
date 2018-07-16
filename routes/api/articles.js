b67a04436b59553c8ad6732b73cd3722fad16243
Ben <bwisler95@gmail.com>
2018-07-16 07:19:03 -0400

    ffwfwe

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -1,24 +1,24 @@
-//`/api/articles` (get) - your components will use this to query 
+//`/api/articles` (get) - your components will use this to query
 // MongoDB for all saved articles
 
-//`/api/articles` (post) - your components will use this 
+//`/api/articles` (post) - your components will use this
 // to save an article to the database
 
-//`/api/articles` (delete) - your components will use this to 
+//`/api/articles` (delete) - your components will use this to
 // delete a saved article in the database
 
-//`*` (get) - will load your single HTML page (with ReactJS) in 
+//`*` (get) - will load your single HTML page (with ReactJS) in
 // `client/build/index.html`. Make sure you put this after all other GET routes
 const router = require("express").Router();
 const articlesController = require("./../../controllers/articlesController");
 
 // Matches withhttp://localhost:3000 "/api/books"
-router.route("/")
+router
+  .route("/")
   // .get(articlesController.findAll)
-  .post(articlesController.saveArticle)
-  router.route("/saved")
-  .get(articlesController.findAll)
-  // .post(articlesController.saveArticle)
+  .post(articlesController.saveArticle);
+router.route("/saved").get(articlesController.findAll);
+// .post(articlesController.saveArticle)
 
 // Matches with "/api/books/:id"
 router
@@ -26,5 +26,6 @@ router
   // .get(articlesController.findById)
   // .post(articlesController.save)
   .delete(articlesController.delete);
-
+  
+router.route("*").get("client/build/index.html");
 module.exports = router;
